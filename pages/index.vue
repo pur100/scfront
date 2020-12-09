aaca8a3959a48c086c500bf778d87a24507befe7
Hugo MANCINI <h.g.mancini@gmail.com>
2020-12-09 11:18:38 +0100

    aa

--- pages/index.vue
+++ pages/index.vue
@@ -279,9 +279,10 @@ export default {
               company_zip: this.company_zip,
               company_city: this.company_city,
               message: this.message
+              this.waitingResponse = false;
             })
           this.response_contact = response
-          this.waitingResponse = false;
+
 
           this.email = ""
           this.siren = ""
